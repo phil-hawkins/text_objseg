9279adb560a90bacad31332c69cb990de4df2211
Phil <phil.a.hawkins@gmail.com>
2018-07-11 14:47:31 +1000

    add batch runners

---
+++ exp_train_lowres.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+
+#PBS -N train_lowres
+#PBS -l ncpus=1
+#PBS -l ngpus=1
+#PBS -q saivt_igpu
+#PBS -l gputype=M40
+#PBS -l mem=16GB 
+#PBS -l walltime=1:00:00
+#PBS -W group_list=SAIVT 
+cd $PBS_O_WORKDIR 
+echo Working directory is $PBS_O_WORKDIR
+
+# Print some other environment information
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+echo This job runs on the following processors:
+NODES=`cat $PBS_NODEFILE`
+echo $NODES
+echo Queue: $PBS_QUEUE
+# Compute the number of processors
+NPROCS=`wc -l < $PBS_NODEFILE`
+echo This job has allocated $NPROCS nodes
+echo -----
+
+
+# put module loads here 
+module load tensorflow/1.4.1-gpu-m40-foss-2016a-python-3.5.1
+
+export PYTHONPATH=.:$PYTHONPATH
+python exp-referit/init_referit_seg_lowres_from_det.py && python exp-referit/exp_train_referit_seg_lowres.py 0
---
+++ exp_train_referit_det.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+
+#PBS -N train_lang
+#PBS -l ncpus=1
+#PBS -l ngpus=1
+#PBS -q saivt_igpu
+#PBS -l gputype=M40
+#PBS -l mem=16GB 
+#PBS -l walltime=10:00:00
+#PBS -W group_list=SAIVT 
+cd $PBS_O_WORKDIR 
+echo Working directory is $PBS_O_WORKDIR
+
+# Print some other environment information
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+echo This job runs on the following processors:
+NODES=`cat $PBS_NODEFILE`
+echo $NODES
+echo Queue: $PBS_QUEUE
+# Compute the number of processors
+NPROCS=`wc -l < $PBS_NODEFILE`
+echo This job has allocated $NPROCS nodes
+echo -----
+
+
+# put module loads here 
+module load tensorflow/1.4.1-gpu-m40-foss-2016a-python-3.5.1
+
+export PYTHONPATH=.:$PYTHONPATH
+python exp-referit/exp_train_referit_det.py 0
